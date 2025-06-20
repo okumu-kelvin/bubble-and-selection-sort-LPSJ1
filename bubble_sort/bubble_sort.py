e6983ab43e1b697d5d2f77ba6167a579aee65bdb
LPSJ1 <victormwakisagu@gmail.com>
2025-06-20 11:57:10 +0300

    ex_complete
    The exercise has been completed with a random generator to make things interesting

--- bubble_sort/bubble_sort.py
+++ bubble_sort/bubble_sort.py
@@ -1,3 +1,20 @@
+import random
+
 def bubble_sort(unsorted_list):
     # TODO: Implement bubble sort
-    pass
+
+    number = len(unsorted_list)
+    for i in range(number):
+        for j in range(0, number - i - 1):
+            if unsorted_list[j] > unsorted_list[j +1]:
+                unsorted_list[j], unsorted_list[j+1] = unsorted_list[j+1], unsorted_list[j]
+
+    return unsorted_list  
+    
+ran_array= [random.randint(1,1000) for _ in range(20)]
+print("Unsorted array:", ran_array)
+
+sorted_array=bubble_sort(ran_array)
+print("Sorted array:", sorted_array)
+    
+
--- selection_sort/selection_sort.py
+++ selection_sort/selection_sort.py
@@ -1,3 +1,20 @@
-def selection_sort(arr):
+import random
+
+def selection_sort(array):
     # TODO: Implement selection sort
-    pass
+    number = len(array)
+    for i in range(number):
+       min_index = i
+       for j in range(i + 1, number):
+           if array[j] < array[min_index]:
+               min_index = j
+
+       array[i], array[min_index] = array[min_index], array[i]
+    
+    return array
+
+randie_array = [random.randint(1,15) for _ in range(20)]
+print("Unsorted:",randie_array)
+
+sorted_array = selection_sort(randie_array)
+print("Sorted:", sorted_array)
